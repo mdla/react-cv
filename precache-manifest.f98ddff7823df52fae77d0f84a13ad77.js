ad6dfd6413869450b79d40fcc5ab59228e6abd27
Mauro Dario Luna Ayala <mauro@MacMauro.local>
2020-10-16 10:15:11 -0300

    Updates

---
+++ precache-manifest.f98ddff7823df52fae77d0f84a13ad77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31930ada68696aafbe40750e337c1172",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "ac4202aa6dece5f69033",
+    "url": "/react-cv/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8691999cd2c658220814",
+    "url": "/react-cv/static/js/2.f7d648be.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/react-cv/static/js/2.f7d648be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac4202aa6dece5f69033",
+    "url": "/react-cv/static/js/main.fa3e9e7d.chunk.js"
+  },
+  {
+    "revision": "453fcf6a3c36c5ab9638",
+    "url": "/react-cv/static/js/runtime-main.5f2c705c.js"
+  }
+]);
