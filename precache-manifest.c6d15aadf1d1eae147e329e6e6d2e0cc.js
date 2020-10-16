483069246377141a343059a47f7fb27933cb7013
Mauro Dario Luna Ayala <mdla.2004@gmail.com>
2020-10-15 22:40:36 -0300

    Updates

---
+++ precache-manifest.c6d15aadf1d1eae147e329e6e6d2e0cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52308a72634c03f73c9a90f15344a6dd",
+    "url": "/react-cv/index.html"
+  },
+  {
+    "revision": "b12c86cba0b2bc9f2814",
+    "url": "/react-cv/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8691999cd2c658220814",
+    "url": "/react-cv/static/js/2.f7d648be.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/react-cv/static/js/2.f7d648be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12c86cba0b2bc9f2814",
+    "url": "/react-cv/static/js/main.17d057fd.chunk.js"
+  },
+  {
+    "revision": "453fcf6a3c36c5ab9638",
+    "url": "/react-cv/static/js/runtime-main.5f2c705c.js"
+  }
+]);
